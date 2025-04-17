54b7d1164f358631a5bae8bffd7ded5da87e470b
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2025-04-17 00:46:07 -0300

    first commit

---
+++ PokeDex/PokeDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeDex
+//
+//  Created by Matheus  Torres on 17/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
